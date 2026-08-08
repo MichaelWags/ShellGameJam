71b05f7fd657362f33a4d948edc219a97d4a67eb
mhwagoner <michaelwagoner1971@gmail.com>
2026-08-07 17:37:54 -0700

    figuring out attack visuals

--- Assets/Scripts/Attack.cs
+++ Assets/Scripts/Attack.cs
@@ -8,9 +8,9 @@ public class Attack : MonoBehaviour
     [SerializeField] private MeshFromPolygonCollider2D meshFromPolygonCollider;
     private PolygonCollider2D polygonCollider2D;
     private LineRenderer lr;
-    private Material mat;
+    private MeshRenderer mr;
     public float damageAmount = 1;
-    private float alphaRate = 0.01f;
+    private float alphaRate = 0.5f;
     private float newAlphaPercent = 1;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -18,18 +18,17 @@ public class Attack : MonoBehaviour
     {
         polygonCollider2D = GetComponent<PolygonCollider2D>();
         lr = GetComponent<LineRenderer>();
-        mat = GetComponent<MeshRenderer>().material;
+        mr = GetComponent<MeshRenderer>();
         meshFromPolygonCollider.CreateMesh();
         meshFromPolygonCollider.CreateLine();
-
-        Disappear();
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Debug.Log(newAlphaPercent);
+        Debug.Log(newAlphaPercent);
         newAlphaPercent -= alphaRate * Time.deltaTime;
+        
         //fade lines
         Gradient gradient = lr.colorGradient;
         GradientAlphaKey[] alphaKeys = gradient.alphaKeys;
@@ -39,10 +38,16 @@ public class Attack : MonoBehaviour
         }
         gradient.SetKeys(gradient.colorKeys, alphaKeys);
         lr.colorGradient = gradient;
-        
-        //fade mesh renderer material
-        Color newColor = new Color(mat.color.r, mat.color.g, mat.color.b, newAlphaPercent*255);
-        mat.color = newColor;
+
+        if (newAlphaPercent <= 0.55)
+        {
+            mr.enabled = false;
+        }
+
+        if (newAlphaPercent <= 0)
+        {
+            Destroy(gameObject);
+        }
     }
 
     void OnTriggerEnter2D(Collider2D collision)
@@ -53,14 +58,5 @@ public class Attack : MonoBehaviour
             GameObject enemy = collision.gameObject;
             enemy.GetComponent<Enemy>().TakeDamage(damageAmount);
         }
-    }
-
-    private void Disappear()
-    {
-        while (newAlphaPercent > 0)
-        {
-            
-        }
-        //Destroy(gameObject);
     }
 }
